f23d8cf0430fde4a790de12b5048fd23f441fee9
0u812 <0u812@users.noreply.github.com>
2019-04-12 12:56:50 -0700

    update repressilator colors

--- gallery/repressilator_animate/repressilator_figure.py
+++ gallery/repressilator_animate/repressilator_figure.py
@@ -11,24 +11,27 @@ __license__ = 'MIT'
 __version__ = '1.0'
 
 # Initialize Parts
-# tetr  is orange [1.00, 0.75, 0.17]
-plac_color = (0.38, 0.65, 0.87)
-ptet_color = (1.00, 0.75, 0.17)
+blue = (0.38, 0.65, 0.87)
+bright_orange = (1.00, 0.75, 0.17)
+red = tuple(x/256.+0.1 for x in (214, 39, 40))
+plac_color = blue
+ptet_color = bright_orange
+pci_color = red
 plac = {'name':'P_lac', 'start':1, 'end':10, 'type':'Promoter', 'opts': {'color':plac_color}}
 rbs1 = {'name':'RBS', 'start':11, 'end':20, 'type':'RBS', 'opts':{'linewidth': 0, 'color':[0.0, 0.0, 0.0]}}
 tetr = {'name':'tetR', 'start':21, 'end':40, 'type':'CDS', 'opts':{'label': 'tetR', 'fontsize': 8,  'label_y_offset': 0, 'label_x_offset': -2, 'label_style':'italic', 'color':ptet_color}}
 term1 = {'name':'Term', 'start':41, 'end':55, 'type':'Terminator'}
-pgamma = {'name':'P_gamma', 'start':56, 'end':65, 'type':'Promoter', 'opts': {'color':[0.38, 0.65, 0.87]}}
+pgamma = {'name':'P_gamma', 'start':56, 'end':65, 'type':'Promoter', 'opts': {'color':pci_color}}
 rbs2 = {'name':'RBS', 'start':66, 'end':75, 'type':'RBS', 'opts':{'linewidth': 0, 'color':[0.0, 0.0, 0.0]}}
 laci = {'name':'lacI', 'start':76, 'end':95, 'type':'CDS', 'opts':{'label': 'lacI', 'fontsize': 8,  'label_y_offset': 0, 'label_x_offset': -2, 'label_style':'italic', 'color':plac_color}}
 term2 = {'name':'Term', 'start':96, 'end':110, 'type':'Terminator'}
 ptet = {'name':'P_tet', 'start':111, 'end':120, 'type':'Promoter', 'opts': {'color':ptet_color}}
 rbs3 = {'name':'RBS', 'start':121, 'end':130, 'type':'RBS', 'opts':{'linewidth': 0, 'color':[0.0, 0.0, 0.0]}}
-gamma = {'name':'gamma', 'start':131, 'end':150, 'type':'CDS', 'opts':{'label': 'cI', 'fontsize': 8, 'label_y_offset': 0, 'label_x_offset': -1, 'label_style':'italic', 'color':[0.38, 0.65, 0.87]}}
+gamma = {'name':'gamma', 'start':131, 'end':150, 'type':'CDS', 'opts':{'label': 'cI', 'fontsize': 8, 'label_y_offset': 0, 'label_x_offset': -1, 'label_style':'italic', 'color':pci_color}}
 term3 = {'name':'Term', 'start':151, 'end':165, 'type':'Terminator'}
 
 lac_repress = {'from_part':laci, 'to_part':plac, 'type':'Repression', 'opts':{'linewidth':1, 'color':plac_color}}
-gamma_repress = {'from_part':gamma, 'to_part':pgamma, 'type':'Repression', 'opts':{'linewidth':1, 'color':[0.38, 0.65, 0.87]}}
+gamma_repress = {'from_part':gamma, 'to_part':pgamma, 'type':'Repression', 'opts':{'linewidth':1, 'color':pci_color}}
 tet_repress = {'from_part':tetr, 'to_part':ptet, 'type':'Repression', 'opts':{'linewidth':1, 'color':ptet_color}}
 
 def plot_construct(ax, t, ymtet, ymlac, ymgamma, ytet, ylac, ygamma):
@@ -38,13 +41,11 @@ def plot_construct(ax, t, ymtet, ymlac, ymgamma, ytet, ylac, ygamma):
 	# Set color for each of the CDSs
 	tetr['opts']['color'] = [rescale(1 - expression(ymlac[tind], exp_lims), (x, 1.0)) for x in ptet_color]
 	laci['opts']['color'] = [rescale(1 - expression(ymlac[tind], exp_lims), (x, 1.0)) for x in plac_color]
-	gamma['opts']['color'] = [rescale(1 - expression(ymgamma[tind], exp_lims), (0.38, 1.0)),
-								rescale(1 - expression(ymgamma[tind], exp_lims), (0.65, 1.0)),
-								rescale(1 - expression(ymgamma[tind], exp_lims), (0.87, 1.0))]
+	gamma['opts']['color'] = [rescale(1 - expression(ymlac[tind], exp_lims), (x, 1.0)) for x in pci_color]
 	# Set transparency for each of the regulatory lines
 	lac_repress['opts']['color'] = [*plac_color,
 								rescale(repression(ylac[tind], 2.0, 8), (0.2, 1.0))]
-	gamma_repress['opts']['color'] = [0.38, 0.65, 0.87,
+	gamma_repress['opts']['color'] = [*pci_color,
 								rescale(repression(ygamma[tind], 2.0, 8), (0.2, 1.0))]
 	tet_repress['opts']['color'] = [*ptet_color,
 								rescale(repression(ytet[tind], 2.0, 8), (0.2, 1.0))]
